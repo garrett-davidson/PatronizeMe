2ae2ee6bd63a72d4747a03ac7164c61dda757f66
Garrett Davidson <gdavids@purdue.edu>
2018-02-01 12:59:27 -0500

    Fix project owner -> User relationship

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
-  belongs_to :owner, inverse_of: :projects
+  belongs_to :owner, class_name: :User, inverse_of: :projects
   has_many :issues, inverse_of: :project
 end
