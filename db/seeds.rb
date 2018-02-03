7b35d06751131ca343ddfff3dec59469c942e5bf
Garrett Davidson <gdavids@purdue.edu>
2018-02-03 14:03:14 -0500

    Add seed fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'active_record/fixtures'
+
+ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), 'users')
+ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), 'projects')
+ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), 'issues')
+ActiveRecord::FixtureSet.create_fixtures(Rails.root.join('test', 'fixtures'), 'issue_transactions')
