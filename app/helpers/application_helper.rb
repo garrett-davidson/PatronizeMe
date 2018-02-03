c97351ada45903d1fca5bce52ce9d45a162b34d2
Garrett Davidson <gdavids@purdue.edu>
2018-02-03 16:23:23 -0500

    Clean up profile picture fetching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def profile_picture_path(user)
+    prefix = 'users/'
+    profile_picture_name = '/profile.png'
+
+    path = prefix + user.id.to_s + profile_picture_name
+
+    asset_path path
+  rescue Sprockets::Rails::Helper::AssetNotFound
+    asset_path('users/default/profile.png')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,9 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :projects, inverse_of: :owner
   has_many :issue_transactions, inverse_of: :user
-
-  def profile_picture_path
-    prefix = 'users/'
-    profile_picture_name = '/profile.png'
-
-    path = prefix + id.to_s + profile_picture_name
-
-    ActionController::Base.helpers.asset_path path
-  rescue Sprockets::Rails::Helper::AssetNotFound
-    ActionController::Base.helpers.asset_path('users/default/profile.png')
-  end
 end
