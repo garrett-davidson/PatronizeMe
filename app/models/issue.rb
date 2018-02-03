75bb40b5ee15f2ff1e23b4fa5d301cfa89272081
Garrett Davidson <gdavids@purdue.edu>
2018-02-03 14:02:26 -0500

    Set up supporters model associations

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,4 +3,5 @@ class Issue < ApplicationRecord
   belongs_to :parent, class_name: :issue
   has_many :children, class_name: :issue
   has_many :issue_transactions, inverse_of: :issue
+  has_many :supporters, source: :user, through: :issue_transactions
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :owner, class_name: :User, inverse_of: :projects
   has_many :issues, inverse_of: :project
+  has_many :supporters, through: :issues
+  has_many :issue_transactions, through: :issues
+
+  def total_funding
+    issue_transactions.sum('amount')
+  end
 end
