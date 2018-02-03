d7a00161902102b1cd3fdb6a0a93ee37aaadc72c
Garrett Davidson <gdavids@purdue.edu>
2018-02-03 16:10:15 -0500

    Add profile pictures to explore page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :projects, inverse_of: :owner
   has_many :issue_transactions, inverse_of: :user
+
+  def profile_picture_path
+    prefix = 'users/'
+    profile_picture_name = '/profile.png'
+
+    path = prefix + id.to_s + profile_picture_name
+
+    ActionController::Base.helpers.asset_path path
+  rescue Sprockets::Rails::Helper::AssetNotFound
+    ActionController::Base.helpers.asset_path('users/default/profile.png')
+  end
 end
