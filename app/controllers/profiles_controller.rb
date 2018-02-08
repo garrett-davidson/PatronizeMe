44f49f3d09bc16801545de19513114d2e3ee7c21
nmaxey <nmaxey@purdue.edu>
2018-02-08 17:21:01 -0500

    made a couple profile changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,14 @@ class ProfilesController < ApplicationController
       		format.json { render json: @profiles }
     	end
 	end
+	private
+	    # Use callbacks to share common setup or constraints between actions.
+	    def set_profile
+	      @profile = Profile.find(params[:id])
+	    end
+
+	    # Never trust parameters from the scary internet, only allow the white list through.
+	    def project_params
+	      params.fetch(:Profile, {})
+	    end
 end
