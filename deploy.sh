6d362b74fefdd09f8586ff599a4739835d3a0947
nohlson <nohlson@purdue.edu>
2018-02-13 18:18:33 -0500

    added deploy to server script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+    echo "Usage: ./deploy (username on eridu)"
+    exit 1
+fi
+#what happens locally
+if [[ $(hostname) != 'eridu' ]]; then
+    rsync -rvzhe ssh --exclude='bin' `pwd` $1@getpatronizeme.com:/home/pm/serverdeploy/
+    ssh $1@getpatronizeme.com bash "/home/pm/serverdeploy/PatronizeMe/deploy.sh eridu"
+fi
+#what happens remotely
+if [[ $(hostname) == 'eridu' ]]; then
+
+        SERVER_PATH="/home/pm/serverdeploy/PatronizeMe/"
+
+        # Restart server
+        echo 'Restarting server'
+
+        cd $SERVER_PATH
+        pwd
+        bundle install
+        bin/rails server
+
+        # Done
+        echo 'Finished'
+
+
+fi
