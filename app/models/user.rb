b521169e629e097d5dfaec936828bcfc0ed02ccc
shaw65 <shaw65@purdue.edu>
2018-02-15 20:26:25 -0500

    added sign out button functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, omniauth_providers: [:github]
 
   has_many :projects, inverse_of: :owner
   has_many :issue_transactions, inverse_of: :user
