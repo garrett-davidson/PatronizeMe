75e961c8ed45f45fbdd05d3ccc283ca25a2c6072
nmaxey <nmaxey@purdue.edu>
2018-02-17 04:42:40 -0500

    a bunch of UI improvemnts added tables for projects and added in some stuff to see if user is logged in (can be cahnged later if it works differently)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,24 @@
 class ProfilesController < ApplicationController
+  PROJECTS_PER_PAGE = 10
 	def show
 		respond_to do |format|
       		format.html { render :profile }
       		format.json { render json: @profiles }
     	end
 	end
-  	def user
-    	# params[:id] comes from the URL: /users/:id
-    	@user = User.find(params[:id])
-    	@owner = User.find(params[:id])
-  	end
-  	def index
-  		@users = User.all
-  	end
+  def user
+  	@user = User.find(params[:id])
+  	@owner = User.find(params[:id])
+    offset = params.permit(:p)[:p]&.to_i || 1
+    # params[:id] comes from the URL: /users/:id
+    @current_page = offset
+    @page_count = (@user.issue_transactions.count / PROJECTS_PER_PAGE.to_f).ceil
+    offset -= 1
+  end
+  def index
+		@users = User.all
+  end
+  def addfunds
+  end
 
 end
