8773cdb6f46db2670d5d3ac1a2a3539cfe83fd36
nohlson <nohlson@purdue.edu>
2018-02-17 13:46:25 -0500

    another deploy

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,18 @@ if [[ $# < 1 ]]; then
 fi
 #what happens locally
 if [[ $(hostname) != 'eridu' ]]; then
-    rsync -rve ssh --delete --exclude=tmp `pwd` $1@getpatronizeme.com:/home/pm/serverdeploy/
-    ssh $1@getpatronizeme.com bash "/home/pm/serverdeploy/PatronizeMe/deploy.sh eridu"
+    rsync -rve ssh --delete --exclude=tmp `pwd` $1@getpatronizeme.com:/home/pm/serverdeploy$1/
+    ssh $1@getpatronizeme.com bash "/home/pm/serverdeploy$1/PatronizeMe/deploy.sh $1"
 fi
 #what happens remotely
 if [[ $(hostname) == 'eridu' ]]; then
 
-        SERVER_PATH="/home/pm/serverdeploy/PatronizeMe/"
+        SERVER_PATH="/home/pm/serverdeploy$1/PatronizeMe/"
 
         # Restart server
         echo 'Restarting server'
 
         cd $SERVER_PATH
-        chmod -R 777 .
-        chown -R :patronizeme .
         pwd
         bundle install
         bin/rails server
