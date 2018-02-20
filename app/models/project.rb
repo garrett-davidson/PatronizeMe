fba1d8d2471e4cf640bc9d47f60dddb1ea253408
nmaxey <nmaxey@purdue.edu>
2018-02-20 02:19:56 -0500

    fixed the mix up on the profile ui and can edit projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ApplicationRecord
   has_many :issues, inverse_of: :project
   has_many :supporters, through: :issues
   has_many :issue_transactions, through: :issues
+  validates_presence_of :name,:status , :notive => 'Must have a name'
 
   def total_funding
     issue_transactions.sum('amount')
