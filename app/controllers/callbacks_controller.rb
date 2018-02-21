08e01119f8f1e0125173930a3dc604d9ebdf439e
Garrett Davidson <gdavids@purdue.edu>
2018-02-20 19:07:05 -0500

    Fix CallbacksController for GitHub

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,21 +1,13 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
-      # build user object
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
-      # check if user already exists
-
-      begin
-        existing_user = User.find(provider: @user.provider, uid: @user.uid)
-        logger.debug 'true'
-        sign_in_and_redirect existing_user
-      rescue
-        logger.debug @user.email
-        redirect_to edit_user_registration_path(@user)
-
-
-        logger.debug 'false'
-      end
-
+    # check if user already exists
+    existing_user = User.find(provider: @user.provider, uid: @user.uid)
+    logger.debug 'Found existing user'
+    sign_in_and_redirect existing_user
+  rescue ActiveRecord::RecordNotFound
+    logger.debug 'Registering new user'
+    render 'devise/registrations/new'
   end
 end
