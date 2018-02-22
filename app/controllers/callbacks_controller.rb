93e58ee0014e374e65e69c6f98d2caaa1ee8580a
nohlson <nohlson@purdue.edu>
2018-02-22 18:24:10 -0500

    clean callback controller

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,12 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
     # check if user already exists
     if existing_user = User.find_by(provider: @user.provider, uid: @user.uid)
       sign_in existing_user
       redirect_to '/'
-    else ActiveRecord::RecordNotFound
+    else
       @provider = @user.provider
       @uid = @user.uid
       @email = @user.email
