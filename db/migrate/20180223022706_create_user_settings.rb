dd334dbbeeb2232af4f9bd64d316acff6c7307d0
shaw65 <shaw65@purdue.edu>
2018-02-22 21:39:41 -0500

    added default boolean for user settings

--- db/migrate/20180223022706_create_user_settings.rb
+++ db/migrate/20180223022706_create_user_settings.rb
@@ -2,7 +2,7 @@ class CreateUserSettings < ActiveRecord::Migration[5.1]
   def change
     create_table :user_settings do |t|
       t.integer :userid
-      t.boolean :isPublic
+      t.boolean :isPublic, null: false, default: true
 
       t.timestamps
     end
