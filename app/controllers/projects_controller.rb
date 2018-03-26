e1e483881fe369b0e975e40a7c11a78151460a5a
Garrett Davidson <gdavids@purdue.edu>
2018-03-25 20:47:09 -0400

    Fetch github issues in new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,11 +18,19 @@ class ProjectsController < ApplicationController
   def show
   end
 
+  def fetch_github_issues
+    json_issues = JSON.parse(RestClient.get('https://api.github.com/repos/' + @project.link + '/issues'))
+    json_issues.map do |issue_json|
+      issue = Issue.new_from_json issue_json
+      issue.project = @project
+      issue
+    end
+  end
+
   # GET /projects/new
   def new
-    @project = Project.new
-    @response = RestClient.get('https://api.github.com/users/'+ current_user.username + '/repos')
-    logger.debug @response
+    @project = Project.new # TODO: Set project here
+    @project.issues = fetch_github_issues
   end
 
   # GET /projects/1/edit
