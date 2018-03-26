dbe7d54ae1b9a45cfc9eee2747d366edc182f08a
Garrett Davidson <gdavids@purdue.edu>
2018-03-25 23:31:23 -0400

    Fix int in create users migration

--- db/migrate/20180131144149_create_users.rb
+++ db/migrate/20180131144149_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.int :balance, null: false, default: 0
+      t.integer :balance, null: false, default: 0
       t.string :provider
       t.string :uid
       t.string :username
