ec22e3f6fe5dde5ed3948a903f872c17673c3a74
Garrett Davidson <davidson.garrettm@gmail.com>
2018-04-22 16:53:40 -0400

    Set up mail settings

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,18 @@ module PatronizeMe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            '<username>',
+      password:             '<password>',
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+
+#    ActionMailer::Base.default_content_type = 'text/html'
   end
 end
