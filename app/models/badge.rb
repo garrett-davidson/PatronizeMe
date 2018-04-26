5189d14a9bc61b601ce8ec86bcf4b9f0daccf228
nmaxey <nmaxey@purdue.edu>
2018-04-26 17:51:35 -0400

    The badges on the profiles

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,12 @@
 class Badge < ApplicationRecord
   belongs_to :user, inverse_of: :badges
+
+  def find(badge_id)
+    info = $badges.select {|_, b| b['id'] == badge_id}
+    info.first().first().to_s
+  end
+
+  def description(badge_name)
+    $badges[badge_name]['description']
+  end
 end
